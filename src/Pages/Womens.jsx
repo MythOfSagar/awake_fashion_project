553878af458c6855fe68d7d95e17d7de1ab220e7
manojmogal007 <manojmogal07@gmail.com>
2022-12-14 20:31:39 +0530

    add womens page code

--- src/Pages/Womens.jsx
+++ src/Pages/Womens.jsx
@@ -1,8 +1,191 @@
 import React from 'react'
+import './Womens.css'
 
 const Womens = () => {
   return (
-    <div>Womens</div>
+    <div className='womens' >
+        {/* <----------------Navbar start-----------------------> */}
+
+
+        {/* <----------------Navbar end-----------------------> */}
+
+        <div className='corner'>
+          <h3>Deals Corner</h3>
+            <div className='cornerchild'>
+                <div className='cornercard'>
+                  <div>
+                    <h4>Buy 2 at</h4>
+                    <h3>60% off</h3>
+                  </div>
+                </div>
+                <div className='cornercard'>
+                  <div>
+                    <h4>Flat</h4>
+                    <h3>50% off</h3>
+                  </div>
+                </div>
+                <div className='cornercard'>
+                  <div>
+                    <h4>Flat</h4>
+                    <h3>40% off</h3>
+                  </div>
+                </div>
+                <div className='cornerc'>
+                  <div>
+                    <h4>Buy 1 at</h4>
+                    <h3>60% off</h3>
+                    <h4>Buy 2 at</h4>
+                    <h3>40% off</h3>
+                  </div>
+                </div>
+                <div className='cornercard'>
+                  <div>
+                    <h4>flat</h4>
+                    <h3>60% off</h3>
+                  </div>
+                </div>
+                <div className='cornerc'>
+                  <div>
+                    <h4>Buy 1 at</h4>
+                    <h3>60% off</h3>
+                    <h4>Buy 2 at</h4>
+                    <h3>40% off</h3>
+                  </div>
+                </div>
+            </div>
+        </div>
+        <div className='dealsimg'>
+            <img src="https://lmsin.net/cdn-cgi/image/w=1232,q=70,fit=cover/https://70415bb9924dca896de0-34a37044c62e41b40b39fcedad8af927.lmsin.net/LS-Fest/LS-new/desktop-dept-modularblock-oneBytwo1A-Women-29Nov2022.gif" alt="" />
+  
+        </div>
+
+        <div className='winterwear'>
+            <h3>Winterwear Essentials</h3>
+            <div>
+                <div>
+                    <div>
+                        <img src="https://sp.yimg.com/ib/th?id=OPA.1q6AG0YkGNQGbw474C474&o=5&pid=21.1&w=&h=" alt="" />
+                    </div>
+                    <p>from ₹699</p>
+                </div>
+                <div>
+                    <div>
+                        <img src="https://sp.yimg.com/ib/th?id=OPA.jWCKtektwZ7fyQ474C474&o=5&pid=21.1&w=&h=" alt="" />
+                    </div>
+                    <p>from ₹799</p>
+                </div>
+                <div>
+                    <div>
+                        <img src="https://sp.yimg.com/ib/th?id=OPA.759z%2fpBOCR2IfQ474C474&o=5&pid=21.1&w=&h=" alt="" />
+                    </div>
+                    <p>from ₹999</p>
+                </div>
+                <div>
+                    <div>
+                        <img src="https://sp.yimg.com/ib/th?id=OPA.IMe0ungf4za01A474C474&o=5&pid=21.1&w=&h=" alt="" />
+                    </div>
+                    <p>from ₹1299</p>
+                </div>
+                <div>
+                    <div>
+                        <img src="https://sp.yimg.com/ib/th?id=OPA.JfatmIIsAy97lQ474C474&o=5&pid=21.1&w=&h=" alt="" />
+                    </div>
+                    <p>from ₹729</p>
+                </div>
+                <div>
+                    <div>
+                        <img src="https://sp.yimg.com/ib/th?id=OPA.iCXTyV%2fqyeN1Xw474C474&o=5&pid=21.1&w=&h=" alt="" />
+                    </div>
+                    <p>from ₹859</p>
+                </div>
+            </div>
+        </div>
+        <div className='shopnow'>
+            <img src="https://lmsin.net/cdn-cgi/image/w=1232,q=70,fit=cover/https://70415bb9924dca896de0-34a37044c62e41b40b39fcedad8af927.lmsin.net/LS-Fest/LS-new/desktop-dept-10modblock-oneBythree-A-Women-06Dec2022.gif" alt="" />
+        </div>
+        <div className='firsttime'> 
+            <img src="https://lmsin.net/cdn-cgi/image/w=1232,q=70,fit=cover/https://70415bb9924dca896de0-34a37044c62e41b40b39fcedad8af927.lmsin.net/LS-Fest/LS-new/desktop-women-9modblock-StripBanner2-D-06Dec2022.gif" alt="" />
+        </div>
+        <div className='ethnicwear'>
+            <h3>Trending Ethnic Wear</h3>
+                <div>
+                    <div style={{backgroundImage:'url(https://sp.yimg.com/ib/th?id=OPA.GM6hLQoVCh6N2Q474C474&o=5&pid=21.1&w=&h=)'}} >
+                        <div className='textdiv'>
+                            <p>Everyday Kurtas</p>
+                            <p>Under ₹859</p>
+                        </div>
+                    </div>
+                    <div style={{backgroundImage:'url(https://sp.yimg.com/ib/th?id=OPA.md3cSCT%2fE5hWOg474C474&o=5&pid=21.1&w=&h=)'}} >
+                        <div className='textdiv'>
+                            <p>Everyday Kurtas</p>
+                            <p>Under ₹1299</p>
+                        </div>
+                    </div>
+                    <div style={{backgroundImage:'url(https://sp.yimg.com/ib/th?id=OPA.vOufU%2bPnFR3PPg474C474&o=5&pid=21.1&w=&h=)'}} >
+                        <div className='textdiv'>
+                            <p>Everyday Kurtas</p>
+                            <p>Under ₹2599</p>
+                        </div>
+                    </div>
+                    <div style={{backgroundImage:'url(https://sp.yimg.com/ib/th?id=OPA.x3gQh1rF%2fefZnQ474C474&o=5&pid=21.1&w=&h=)'}} >
+                        <div className='textdiv'>
+                            <p>Everyday Kurtas</p>
+                            <p>Under ₹2699</p>
+                        </div>
+                    </div>
+                    <div style={{backgroundImage:'url(https://sp.yimg.com/ib/th?id=OPA.Pw%2bHMI4keDFEbQ474C474&o=5&pid=21.1&w=&h=)'}} >
+                        <div className='textdiv'>
+                            <p>Everyday Kurtas</p>
+                            <p>Under ₹799</p>
+                        </div>
+                    </div>
+                    <div style={{backgroundImage:'url(https://sp.yimg.com/ib/th?id=OPA.Lm7T0LMrIDdQvQ474C474&o=5&pid=21.1&w=&h=)'}} >
+                        <div className='textdiv'>
+                            <p>Everyday Kurtas</p>
+                            <p>Under ₹899</p>
+                        </div>
+                    </div>
+                </div>
+        </div>
+        <div className='flashsale'>
+            <h3>Flash Sale For Today</h3>
+            <img src="https://lmsin.net/cdn-cgi/image/w=1232,q=70,fit=cover/https://70415bb9924dca896de0-34a37044c62e41b40b39fcedad8af927.lmsin.net/LS-Fest/LS-new/desktop-women-modularblock1-A-13Dec2022A.gif" alt="" />
+        </div>
+        <div className='toppics'>
+            <h3>Top Picks</h3>
+            <div>
+                <img src="https://lmsin.net/cdn-cgi/image/w=500,q=70,fit=cover/https://70415bb9924dca896de0-34a37044c62e41b40b39fcedad8af927.lmsin.net/LS-Fest/LS-new/desktop-Women-modblock-oneBythree-D-24Nov2022.jpg" alt="" />
+                <img src="https://lmsin.net/cdn-cgi/image/w=500,q=70,fit=cover/https://70415bb9924dca896de0-34a37044c62e41b40b39fcedad8af927.lmsin.net/LS-Fest/LS-new/desktop-Women-modblock-oneBythree-E-24Nov2022.jpg" alt="" />
+                <img src="https://lmsin.net/cdn-cgi/image/w=500,q=70,fit=cover/https://70415bb9924dca896de0-34a37044c62e41b40b39fcedad8af927.lmsin.net/LS-Fest/LS-new/desktop-Women-modblock-oneBythree-F-24Nov2022.jpg" alt="" />
+            </div>
+        </div>
+        <div className='onlyimg'>
+            <img src="https://lmsin.net/cdn-cgi/image/w=1232,q=70,fit=cover/https://70415bb9924dca896de0-34a37044c62e41b40b39fcedad8af927.lmsin.net/LS-Fest/LS-new/desktop-dept-12modblock-oneBythree-A-StripBanner1-24Nov2022.jpg" alt="" />
+
+        </div>
+        <div className='trendybottom' >
+            <div>
+                <div className='bottom' style={{backgroundImage:'url(https://tse4.mm.bing.net/th?id=OIP.8dZ5rPg86R5k_b3IDnaW7AHaHa&pid=Api&P=0)'}}>
+                    <p>Jeans and jeggings</p>
+                </div>
+                <div className='bottom' style={{backgroundImage:'url(https://sp.yimg.com/ib/th?id=OPA.x5crT7slRcQJMQ474C474&o=5&pid=21.1&w=&h=)'}}>
+                    <p>Trousers and pants</p>
+                </div>
+                <div className='bottom' style={{backgroundImage:'url(https://sp.yimg.com/ib/th?id=OPA.NzCKv6ZXQicKvw474C474&o=5&pid=21.1&w=&h=)'}}>
+                    <p>Activewear leggings</p>
+                </div>
+                <div className='bottom' style={{backgroundImage:'url(https://sp.yimg.com/ib/th?id=OPA.zrlA7UyzgIysUw474C474&o=5&pid=21.1&w=&h=)'}}>
+                    <p>joggers and tracks</p>
+                </div>
+                <div className='bottom' style={{backgroundImage:'url(https://tse3.mm.bing.net/th?id=OIP._t67ZGsgrQAYord6Bjr4jAHaHa&pid=Api&P=0)'}}>
+                    <p>Ethnic leggings</p>
+                </div>
+                <div className='bottom' style={{backgroundImage:'url(https://tse4.mm.bing.net/th?id=OIP.5Mbni6jKVdcVNj_Up39iRQHaHa&pid=Api&P=0)'}}>
+                    <p>Ethnic pants & Palazzos</p>
+                </div>
+            </div>
+        </div>
+    </div>
   )
 }
 
