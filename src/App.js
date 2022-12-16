a8ddc4366add8958d15d50e4d0c8d0291e93df00
Avinash Patel <avinashpatel9755@gmail.com>
2022-12-16 21:46:07 +0530

    Complete Home Page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import Footer from "./Components/Footer/Footer";
+import HomePage from "./Components/HomePage/HomePage";
 import Navbar from "./Components/Navbar/Navbar";
 import Womens from "./Pages/Womens";
 function App() {
   return (
     <>
       <Navbar />
+      <HomePage />
       <Footer />
     </>
   );
