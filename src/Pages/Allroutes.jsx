d566014364e93bebd406bba3373bbcbe48750f4b
Manoj Mogal <107462443+manojmogal007@users.noreply.github.com>
2022-12-17 18:15:53 +0530

    Update Allroutes.jsx
    add

--- src/Pages/Allroutes.jsx
+++ src/Pages/Allroutes.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
 import { Route, Routes } from 'react-router-dom'
 import HomePage from '../Components/HomePage/HomePage'
+import Womens from './Womens'
+import Wproducts from './Wproducts'
 const Allroutes = () => {
   return (
     <div>
     <Routes>
-    
+            <Route path='/womensproduct' element={<Wproducts/>}></Route>
+        <Route path='/womens' element={<Womens/>}></Route>
         <Route path='/' element={<HomePage/>}></Route>
     </Routes>
     </div>
