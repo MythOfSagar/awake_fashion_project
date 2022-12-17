3d13f8f864713150ba6a1ef65ada72e2026cf25b
Manoj Mogal <107462443+manojmogal007@users.noreply.github.com>
2022-12-17 17:58:24 +0530

    Update Allroutes.jsx
    a

--- src/Pages/Allroutes.jsx
+++ src/Pages/Allroutes.jsx
@@ -1,9 +1,6 @@
 import React from 'react'
 import { Route, Routes } from 'react-router-dom'
-import Basket from '../Components/Basket/basket'
-import PaymentResponse from '../Components/Basket/paymentResponse'
-import PaymentSuccess from '../Components/Basket/PaymentSuccess'
-import Shipping from '../Components/Basket/shipping'
+import Shipping from '../Components/Basket/shipping''
 import { InitialFocus } from '../Components/Basket/signIn'
 import Womens from './Womens'
 import Wproducts from './Wproducts'
@@ -12,13 +9,9 @@ const Allroutes = () => {
   return (
     <div>
     <Routes>
-        <Route path='/signIn' element={<InitialFocus></InitialFocus>}></Route>
         <Route path='/womensproduct' element={<Wproducts/>}></Route>
         <Route path='/womens' element={<Womens/>}></Route>
         <Route path='/shipping' element={<Shipping></Shipping>}></Route>
-        <Route path='/basket' element={<Basket></Basket>}></Route>
-        <Route path='/paymentsuccess' element={<PaymentSuccess></PaymentSuccess>}></Route>
-        <Route path='/paymentresponse/:title/:src' element={<PaymentResponse></PaymentResponse>}></Route>
     </Routes>
     </div>
   )
