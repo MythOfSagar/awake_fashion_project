626a7a5f30462bb973c93524d10db39cd12770e6
Avinash Patel <106015273+AvinashPatel15@users.noreply.github.com>
2022-12-21 12:28:40 +0530

    Delete publicc directory
    Deleted

--- publicc/src/redux/store.js
+++
@@ -1,7 +0,0 @@
-import {legacy_createStore,compose,applyMiddleware} from 'redux'
-import thunk from 'redux-thunk'
-import { reducer } from './reducer';
-
-const composeEnhancers =window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-
-export const store = legacy_createStore(reducer,composeEnhancers(applyMiddleware(thunk)));
